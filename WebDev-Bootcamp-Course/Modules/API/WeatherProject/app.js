f4394c7d8693c56e295b337b58eb438f39d372ac
LaurentBaj <laurent_baj@hotmail.com>
2020-07-07 23:07:50 +0200

    Finished Weather API
    Gave it a textbar where the user can enter a city name and then recieve info about the weather there + appropriate weather icon

--- WebDev-Bootcamp-Course/Modules/API/WeatherProject/app.js
+++ WebDev-Bootcamp-Course/Modules/API/WeatherProject/app.js
@@ -1,15 +1,53 @@
 const express = require("Express"); 
 const app = express(); 
 
-const https = require("https"); // Does not need to be installed 
+const bodyParser = require("body-parser")
+app.use(bodyParser.urlencoded({extended: true})); 
+
+const https = require("https"); // Does not need to be installed with npm
 
 app.get("/", function(req, res) {
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Mombasa&appid=80c8a3b4c1a2376a82e114bc6eab4fc3&units=metric";
-    https.get(url, function(response) {
-        console.log(response);
-    }); 
+    res.sendFile(__dirname + "/index.html"); 
 });
 
-app.listen(3000, function() {
-    console.log("Server is sunning on port 3000");
+app.post("/", function(req, res) {
+    
+    const query = req.body.cityName; // <Working with the HTML
+    const apiKey = "80c8a3b4c1a2376a82e114bc6eab4fc3"
+    const unit = "metric";
+
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit; // I've altered the parameters
+
+    https.get(url, function(response) {
+        console.log(response.statusCode);
+
+        response.on("data", function(data) {
+
+            const weatherData = JSON.parse(data);
+
+            const weatherDescription = weatherData.weather[0].description; 
+            const name = weatherData.name; 
+            const wind = weatherData.wind.deg; 
+            const temp = weatherData.main.temp; 
+
+            // Getting the proper icon
+            const icon = weatherData.weather[0].icon; 
+            const imageUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+            
+            console.log(weatherDescription);
+            console.log(name);
+            console.log(wind);
+            console.log(temp);
+
+            res.write("<h1>The temperature in " + name + " is " + temp + "degrees Celcius.</h1>"); 
+            res.write("<p>The name of the city is " + name + "</p>"); 
+            res.write("<img src=" + imageUrl +">"); 
+            res.send(); 
+        });
+    }); 
+}); 
+
+
+app.listen(3000, function() { 
+    console.log("Server is running on port 3000");
 });
