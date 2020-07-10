70bb1f92168196f04e89f61876046ce9ade268be
LaurentBaj <laurent_baj@hotmail.com>
2020-07-10 17:48:19 +0200

    Finished Module on EJS

--- WebDev-Bootcamp-Course/Modules/EJS/todolist-v1/app.js
+++ WebDev-Bootcamp-Course/Modules/EJS/todolist-v1/app.js
@@ -1,41 +1,59 @@
 const express = require("express"); 
-const bodyParser = require("body-parser"); 
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js"); 
+
+const { request } = require("express");
 
 const app = express(); 
 
-let item = ""; 
+const items = ["Buy Food", "Cook Food", "Eat Food"]; 
+const workItems = []; 
 
 app.use(bodyParser.urlencoded({extended: true})); 
+app.use(express.static("public")); 
 
 app.set("view engine", "ejs"); 
 
 app.get("/", function(req, res) {
-
-    let today = new Date(); 
-
-    const options = {
-        weekday: "long", 
-        day: "numeric", 
-        month: "long"
-    }; 
     
-    let day = today.toLocaleDateString("en-US", options); 
+    const day = date.getDate(); 
 
     res.render("list", {
-        kindOfDay: day, 
-        newListItem: data
+        listTitle: day, 
+        newListItems: items
     }); 
-
 }); 
 
 app.post("/", function(req, res) {
-    
-    const item = req.body.newItem;  
-    
-    res.redirect("/"); 
+
+    let item = req.body.newItem; 
+
+    if(req.body.list === "Work") {
+        workItems.push(item); 
+        res.redirect("/work"); 
+    } else {
+        items.push(item);
+        res.redirect("/"); 
+    } 
 
 }); 
 
+app.get("/work", function(req, res) {
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.post("/work", function(req, res) {
+    let item = req.body.newItem; 
+    workItems.push(item); 
+    res.redirect("/work"); 
+}); 
+
+
+app.get("/about", function(req, res) {
+    res.render("about"); 
+}); 
+
+
 app.listen(3000, function() {
     console.log("Server runs on port 3000s");
-})
+});
